79cd8a98cbc9a44b2b9cef9e42923f9d4be742e2
zhaoyk1981 <zhaoyongkun@hotmail.com>
2019-07-07 12:25:35 +0800

    t

--- DaSongERP/WebApp/js/pages/orderlist.js
+++ DaSongERP/WebApp/js/pages/orderlist.js
@@ -1,7 +1,15 @@
 ﻿define(['jquery', 'kyle_toolkit_enhance', 'kyle_toolkit_model', 'kyle_toolkit_validation'], function ($, enhance, model, validation) {
     let btnSearch_click = function () {
         let s = encodeURI($.trim($('#TxtSearch').val()));
-        window.location = '/Order?search=' + s;
+        let url = '/Order?search=' + s;
+        //let shouhou = $('.shouhou-filters');
+        //if (shouhou.length === 1) {
+        //    var f = $('#Chk已售后但未完结').prop('checked') ? 'true' : 'false';
+        //    var m = $('#Chk只看我的').prop('checked') ? 'true' : 'false';
+
+        //}
+
+        window.location = url;
     };
 
     return {
