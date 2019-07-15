3829ec537fe10379c548ac1e77f49127934b2c2e
zhaoyk1981 <zhaoyongkun@hotmail.com>
2019-07-15 23:32:34 +0800

    t

--- DaSongERP/WebApp/js/pages/neworder.js
+++ DaSongERP/WebApp/js/pages/neworder.js
@@ -35,10 +35,6 @@
                     alert('操作失败');
                     $('#BtnSubmit').prop('disabled', false);
                 }
-
-                let copyText = document.getElementById("txtCopy");
-                copyText.select();
-                document.execCommand("copy");
             }
         });
     };
@@ -79,6 +75,38 @@
             }
         });
         return true;
+    };
+
+    let fireKeyEvent = function (el, evtType, keyCode) {
+        var doc = el.ownerDocument,
+            win = doc.defaultView || doc.parentWindow,
+            evtObj;
+        if (doc.createEvent) {
+            if (win.KeyEvent) {
+                evtObj = doc.createEvent('KeyEvents');
+                evtObj.initKeyEvent(evtType, true, true, win, false, false, false, false, keyCode, 0);
+            }
+            else {
+                evtObj = doc.createEvent('UIEvents');
+                Object.defineProperty(evtObj, 'keyCode', {
+                    get: function () { return this.keyCodeVal; }
+                });
+                Object.defineProperty(evtObj, 'which', {
+                    get: function () { return this.keyCodeVal; }
+                });
+                evtObj.initUIEvent(evtType, true, true, win, 1);
+                evtObj.keyCodeVal = keyCode;
+                if (evtObj.keyCode !== keyCode) {
+                    console.log("keyCode " + evtObj.keyCode + " 和 (" + evtObj.which + ") 不匹配");
+                }
+            }
+            el.dispatchEvent(evtObj);
+        }
+        else if (doc.createEventObject) {
+            evtObj = doc.createEventObject();
+            evtObj.keyCode = keyCode;
+            el.fireEvent('on' + evtType, evtObj);
+        }
     };
 
     let timer = null;
